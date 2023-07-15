4e867b763e3365fcb9a8199cd1f44f8463c54bab
lu-css <dossantossilvalucas963@gmail.com>
2023-07-15 17:01:47 -0300

    algumas melhorias

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -65,6 +65,8 @@ class AccountController < ApplicationController
   end
 
   get '/confirm/:code' do |code|
+    unnathenticate!
+
     err_msg = User::ConfirmEmail.new(user_id: @current_user.id).with_code(code).call
 
     if err_msg
--- app/helpers/render_helper.rb
+++ app/helpers/render_helper.rb
@@ -3,10 +3,9 @@
 ## RenderHelper
 module RenderHelper
   def render!(action)
-    caller_file_path = caller_locations(1, 1)[0].path
-    caller_file_name = File.basename(caller_file_path)
-    folder = caller_file_name.sub(/_controller\.rb$/, '')
-
+    folder = caller_locations(1, 1)[0].path
+                                      .then { File.basename _1 }
+                                      .then { _1.sub(/_controller\.rb$/, '') }
     erb :"#{folder}/#{action}"
   end
 
