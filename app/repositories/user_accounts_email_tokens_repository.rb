8771c9e780113027051fb6d0caa830a40338f296
lu-css <dossantossilvalucas963@gmail.com>
2023-07-15 16:09:44 -0300

    refactor: remove comments

--- app/repositories/user_accounts_email_tokens_repository.rb
+++ app/repositories/user_accounts_email_tokens_repository.rb
@@ -10,37 +10,4 @@ MAX_AGE = 24 * 60 * 60
 class UserAccountsEmailTokensRepository < ActiveRecord::Base
   validates_presence_of :user_id, :valid_for
   self.table_name = 'user_accounts_email_tokens'
-  # def self.new_email_token(user)
-  #   valid_for = Time.now + MAX_AGE
-  #   email = UserAccountsEmailTokens.new
-  #   email.user_id = user.id
-  #   email.creted_at = Time.now
-  #   email.valid_for = valid_for
-
-  #   email.save
-
-  #   email
-  # end
-
-  # def self.user_id_from_code(code)
-  #   email = UserAccountsEmailTokens.find_by(id: code)
-
-  #   email.user_id
-  # end
-
-  # def self.token_from_user(user)
-  #   email = UserAccountsEmailTokens.find_by(user_id: user.id)
-
-  #   return nil unless email
-
-  #   email
-  # end
-
-  # def self.email_tokem_from_code(code)
-  #   UserAccountsEmailTokens.find_by(id: code)
-  # end
-
-  # def self.destroy_code(user)
-  #   UserAccountsEmailTokens.where(user_id: user.id).delete_all
-  # end
 end
--- app/repositories/user_accounts_repository.rb
+++ app/repositories/user_accounts_repository.rb
@@ -7,29 +7,4 @@ class UserAccountsRepository < ActiveRecord::Base
   validates_presence_of :user_name, :password, :email
 
   self.table_name = 'user_accounts'
-
-  # def self.user_from_email_password(email, password)
-  #   user = UserAccounts.find_by(email:)
-
-  #   return user if user && BCrypt::Password.new(user.password) == password
-
-  #   nil
-  # end
-
-  # def self.user_by_id(id)
-  #   UserAccounts.find_by(id:)
-  # end
-
-  # def self.new_from_params(params)
-  #   user = UserAccounts.new
-
-  #   user.user_name = params[:user_name]
-  #   user.password = BcryptService.encode_password(params[:password])
-  #   user.email = params[:email]
-  #   user.roles = %w[admin user]
-
-  #   user.save
-
-  #   user
-  # end
 end
